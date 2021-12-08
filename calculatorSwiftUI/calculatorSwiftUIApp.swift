04517e839d886fad9bc73a0247103b72742d9a7b
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2021-12-08 21:20:05 +0200

    Initial Commit

---
+++ calculatorSwiftUI/calculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorSwiftUIApp.swift
+//  calculatorSwiftUI
+//
+//  Created by macbook on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct calculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
